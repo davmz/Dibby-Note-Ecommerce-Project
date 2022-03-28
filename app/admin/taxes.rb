cd0d545d744f545c275f117601f65f80bf60761c
davmz <montanezdavid@gmail.com>
2022-03-28 17:50:08 -0500

    Initialize and associated active admin with the new models created.

---
+++ app/admin/taxes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Tax do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :pst, :gst, :hst, :totaltax, :province_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:pst, :gst, :hst, :totaltax, :province_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @instruments = Instrument.all
+
+    @types = Type.all
+
+    @lessons = Lesson.all
+
+    @provinces = Province.all
+
+    @taxes = Tax.all
   end
 end
