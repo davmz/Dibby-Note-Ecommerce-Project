85eb885be800a76b9a567246aec684fc7bc086a0
davmz <montanezdavid@gmail.com>
2022-03-29 15:50:36 -0500

    Shows image on preview on activeadmin

--- app/admin/instruments.rb
+++ app/admin/instruments.rb
@@ -1,29 +1,12 @@
 ActiveAdmin.register Instrument do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :price, :type_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :price, :type_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
   permit_params :name, :price, :type_id, :image
-
 
   form do |f|
     f.semantic_errors # shows errors on :base
     f.inputs          # builds an input field for every attribute
     f.actions         # adds the 'Submit' and 'Cancel' buttons
     f.inputs do
-      f.input :image, as: :file
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
     end
   end
 end
