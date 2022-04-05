f5f69386e7a88e5f7b157fedda7f1d39a6590f82
davmz <montanezdavid@gmail.com>
2022-04-05 17:03:46 -0500

    Generated aUser model (including secure passwords)

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,5 +1,6 @@
 class Province < ApplicationRecord
   has_one :tax
+  has_many :user
 
   # Validation
   validates :name, :code, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_04_173549) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_05_220156) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -112,6 +112,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_04_173549) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first"
+    t.string "last"
+    t.string "fullname"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
