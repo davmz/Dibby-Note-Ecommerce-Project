2d4a9874d55c0d4379f1bb0433ab730be9f67756
davmz <montanezdavid@gmail.com>
2022-04-05 03:26:59 -0500

    Filter the instrument products to only see the instruments that are new and recently updated.

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,6 +1,14 @@
 class InstrumentsController < ApplicationController
   def index
-    @instruments = Instrument.includes(:type).all
+    if (params.has_key?(:instrument_filter))
+      if (params[:instrument_filter].to_s == "New-Instrument")
+        @instruments = Instrument.where("created_at >= ?", Time.zone.now.beginning_of_day)
+      elsif
+        @instruments = Instrument.where("updated_at >= ?", Time.zone.now.beginning_of_day)
+      end
+    else
+      @instruments = Instrument.includes(:type).all.order("created_at desc")
+    end
   end
 
   def show
