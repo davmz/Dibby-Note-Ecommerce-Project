00a122fbff8e09907453f0cd71580f5de0e352ec
davmz <montanezdavid@gmail.com>
2022-04-26 08:52:51 -0500

    Styling of pages and added more information

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,6 +1,5 @@
 class CheckoutController < ApplicationController
   def create
-    # establish a connection to Stripe and then redirect the user to the payment screen
     instrument = Instrument.find(params[:instrument_id])
 
     if instrument.nil?
@@ -10,29 +9,25 @@ class CheckoutController < ApplicationController
 
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ["card"],
-      success_url: checkout_success_url,
-      cancel_url: checkout_cancel_url,
-      line_items: [
-        name: instrument.name,
+      success_url:          checkout_success_url,
+      cancel_url:           checkout_cancel_url,
+      line_items:           [
+        name:        instrument.name,
         description: instrument.name,
-        amount: instrument.price * 100.to_i, #price * 100.to_i
-        currency: "cad",
-        quantity: 1
+        amount:      instrument.price * 100.to_i, # price * 100.to_i
+        currency:    "cad",
+        quantity:    1
       ]
     )
-    respond_to do | format |
-      format.js #app/views/checkout/create.js.erb
+    respond_to do |format|
+      format.js # app/views/checkout/create.js.erb
     end
   end
 
   def success
-    # we took the customer's money
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
   end
 
-
-  def cancel
-
-  end
+  def cancel; end
 end
