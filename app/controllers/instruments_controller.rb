752f4c00cfde440aa58df3e605f980e45c4b8d3e
davmz <montanezdavid@gmail.com>
2022-04-25 20:02:00 -0500

    Only add to cart on specific instrument pages

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -5,13 +5,13 @@ class InstrumentsController < ApplicationController
   def add_to_cart
     id = params[:id].to_i
     session[:cart] << id unless session[:cart].include?(id)
-    redirect_to instruments_path
+    redirect_to instrument_path(id)
   end
 
   def remove_from_cart
     id = params[:id].to_i
     session[:cart].delete(id)
-    redirect_to instruments_path
+    redirect_to instrument_path(id)
   end
 
   def index
